9b611f8fec52ca80245624f5a6c844117426a16f
Maria19761976 <mpotero1976@gmail.com>
2026-04-16 13:20:19 +0200

    feature:filesdto

--- Documents/Bootcamp/Inditex Gym/gym_lorza/src/main/java/com/inditex/gym_lorza/dto/user/UserRequestDTO.java
+++ Documents/Bootcamp/Inditex Gym/gym_lorza/src/main/java/com/inditex/gym_lorza/dto/user/UserRequestDTO.java
@@ -1,4 +1,23 @@
 package com.inditex.gym_lorza.dto.user;
 
 public class UserRequestDTO {
+    private String name;
+    private String surname;
+    private String dni;
+    private int start_year;
+    private boolean activo;
+    private String image;
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getSurname() { return surname; }
+    public void setSurname(String surname) { this.surname = surname; }
+    public String getDni() { return dni; }
+    public void setDni(String dni) { this.dni = dni; }
+    public int getStart_year() { return start_year; }
+    public void setStart_year(int start_year) { this.start_year = start_year; }
+    public boolean isActivo() { return activo; }
+    public void setActivo(boolean activo) { this.activo = activo; }
+    public String getImage() { return image; }
+    public void setImage(String image) { this.image = image; }
 }
