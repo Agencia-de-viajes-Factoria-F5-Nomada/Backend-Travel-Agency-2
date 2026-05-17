f16de381c4694378d2a5a647d719d88716921aed
Maria19761976 <mpotero1976@gmail.com>
2026-05-17 10:29:03 +0200

    fix: simplify NoOpEmailService removing EmailServiceImpl dependency

--- src/main/java/com/inditex/g1_agencia_viajes/service/NoOpEmailService.java
+++ src/main/java/com/inditex/g1_agencia_viajes/service/NoOpEmailService.java
@@ -1,10 +1,8 @@
 package com.inditex.g1_agencia_viajes.service;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.stereotype.Service;
 
 @Service
-@ConditionalOnMissingBean(EmailServiceImpl.class)
 public class NoOpEmailService implements EmailService {
     @Override
     public void sendBookingConfirmation(Long bookingId) {
